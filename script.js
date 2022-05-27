1d84343b692f9581bbecdc075e82f087adaf4c34
Marco S <marcosantinia12@gmail.com>
2022-05-27 14:19:09 -0700

    Completed JS

--- script.js
+++ script.js
@@ -7,32 +7,27 @@ var ties = 0;
 var loses = 0;
 let gmresult = "";
 
-//Random computer selection 
+//Function for random computer selection
 function computerPlay(){
     let hand = ["Rock", "Paper", "Scissors"];
     let randomChoice = hand[Math.floor(Math.random()* hand.length)];
-    randomChoice = randomChoice.toUpperCase();
-    return randomChoice;
+    computerSelection = randomChoice.toUpperCase();
+    return computerSelection;
 } 
-computerSelection = computerPlay()
 
-//User selection
-// playerSelection = "scissors";
+//Function for user selection through prompt
 function playerSelectionFunct(){
     playerSelection = prompt("Select: rock, paper, scissors?")
     playerSelection = playerSelection.toUpperCase();
     if(playerSelection == "ROCK")
     {
-        playerSelection;
-        console.log("plr slct: " + playerSelection);
+        playerSelection = "ROCK";
     } 
     else if (playerSelection == "PAPER"){
-        playerSelection;
-        console.log(playerSelection);
+        playerSelection = "PAPER";
     } 
     else if (playerSelection == "SCISSORS"){
-        playerSelection;
-        console.log(playerSelection);
+        playerSelection = "SCISSORS";
     } else
     {
         alert("invalid selection");
@@ -41,42 +36,32 @@ function playerSelectionFunct(){
     return playerSelection;
 }
 
-//Run round of RPS
+//Run 1 round & determine round winner
 function playRound(playerSelection, computerSelection){
 
     let outcome = "";
 
     //Winner for different scenarios
-    switch(playerSelection, computerSelection) {
-        case "SCISSORS" , "SCISSORS":
-            outcome = "You Tie! Scissors ties with Scissors";
-            break;
-        case "SCISSORS" , "PAPER":
-            outcome = "You Win! Scissors beats Paper";
-            break;
-        case "SCISSORS" , "ROCK":
-            outcome = "You Lose! Rock beats Scissors";
-            break;
-        case "PAPER" , "SCISSORS":
-            outcome = "You Lose! Scissors beats to Paper";
-            break;
-        case "PAPER" , "PAPER":
-            outcome = "You Tie! Paper ties with Paper";
-            break;
-        case "PAPER" , "ROCK":
-            outcome = "You Win! Paper beats Rock";
-            break;
-        case "ROCK" , "SCISSORS":
-            outcome = "You Win! Rock beats Scissors";
-            break;
-        case "ROCK" , "PAPER":
-            outcome = "You Lose! Paper beats Rock";
-            break; 
-        case "ROCK" , "ROCK":
-            outcome = "You Tie! Rock ties with Rock";
-            break;           
-          // code block
-      }
+
+    if((playerSelection == "SCISSORS") && (computerSelection == "PAPER")){
+        outcome = "You Win! Scissors beats Paper";
+    } else if((playerSelection == "SCISSORS") && (computerSelection == "ROCK")){
+        outcome = "You Lose! Rock beats Scissors";
+    } else if((playerSelection == "PAPER") && (computerSelection == "SCISSORS")){
+        outcome = "You Lose! Scissors beats to Paper";
+    } else if((playerSelection == "PAPER") && (computerSelection == "PAPER")){
+        outcome = "You Tie! Paper ties with Paper";
+    } else if((playerSelection == "PAPER") && (computerSelection == "ROCK")){
+        outcome = "You Win! Paper beats Rock";
+    } else if((playerSelection == "ROCK") && (computerSelection == "SCISSORS")){
+        outcome = "You Win! Rock beats Scissors";
+    } else if((playerSelection == "ROCK") && (computerSelection == "PAPER")){
+        outcome = "You Lose! Paper beats Rock";
+    } else if((playerSelection == "ROCK") && (computerSelection == "ROCK")){
+        outcome = "You Tie! Rock ties with Rock";
+    } else if((playerSelection == "SCISSORS") && (computerSelection == "SCISSORS")){
+        outcome = "You Tie! Scissors ties with Scissors";
+    } 
 
     //Outcome
     return outcome;
@@ -84,57 +69,56 @@ function playRound(playerSelection, computerSelection){
 
 //5 Round game
 function game(){
-    
-//Initial Player selection
-playerSelectionFunct();
 
+    //Call playRound 5 times via loop, and store results to each round aswell as keep counter of W,L,T
     for (let i=0; i<5; i++){
+
+        playerSelectionFunct(); //New Player Selection
+        computerPlay(); // New PC Selection
+
         roundResults.push(playRound(playerSelection, computerSelection))
         console.log("Round " + (i+1));
         console.log("PC Selection: " + computerSelection);
         console.log("Player Selection: " + playerSelection);
         console.log(roundResults[i]);
-
         //call win,loss & tie counters
         console.log("wins: " + winCounter());
         console.log("loses: " + lossCounter());
         console.log("ties: " + tieCounter());
         console.log("---------------")
         computerSelection = computerPlay(); //Change computer selection after round
-        playerSelectionFunct();
     }
 
     let gameBreakdown = ("Wins: " + wins + " | Loses: " + loses + " | Ties: " + ties);
     console.log(gameBreakdown);
-    console.log(gameResult());
+    console.log(gameResult()); //calls gameResult funct. Which is the total 5 round game outcome
 }
 
-//Keep track of wins
-function winCounter(){
-    let rslt = playRound(playerSelection, computerSelection);
-    if(rslt.includes("Win")){
-        wins = wins+1;
+//Win, LOSS, TIE Trackers
+    //Keep track of wins
+    function winCounter(){
+        let rslt = playRound(playerSelection, computerSelection);
+        if(rslt.includes("Win")){
+            wins = wins+1;
+        };
+        return wins;
     };
-    return wins;
-};
-
-//Keep track of loses
-function lossCounter(){
-    let rslt = playRound(playerSelection, computerSelection);
-    if(rslt.includes("Lose")){
-        loses = loses+1;
+    //Keep track of loses
+    function lossCounter(){
+        let rslt = playRound(playerSelection, computerSelection);
+        if(rslt.includes("Lose")){
+            loses = loses+1;
+        };
+        return loses;
     };
-    return loses;
-};
-
-//Keep track of ties
-function tieCounter(){
-    let rslt = playRound(playerSelection, computerSelection);
-    if(rslt.includes("Tie")){
-        ties = ties+1;
+    //Keep track of ties
+    function tieCounter(){
+        let rslt = playRound(playerSelection, computerSelection);
+        if(rslt.includes("Tie")){
+            ties = ties+1;
+        };
+        return ties;
     };
-    return ties;
-};
 
 // Compare W, L, T and return a final result
 function gameResult(){

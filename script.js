759e7e9cdd56906505bf77604146c2277ce35c7b
Marco S <marcosantinia12@gmail.com>
2022-06-15 10:58:37 -0700

    Changed styling, added tallie tracker and selection displays

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let computerSelection = "";
 var roundResults = [];
 var wins = 0;
 var ties = 0;
-var loses = 0;
+var pcWins = 0;
 let gmresult = "";
 
 //Function for random computer selection
@@ -55,29 +55,35 @@ function playRound(playerSelection, computerSelection){
     cmptrSlct = dsplyCmptrSlct(computerSelection);
     console.log("Player Select: " + plyrSlct)
     console.log("Comp Select: " + cmptrSlct)
+    
     let outcome = "";
 
     //Winner for different scenarios
 
-    if((plyrSlct == "SCISSORS") && (computerSelection == "PAPER")){
+    if((plyrSlct === "SCISSORS") && (cmptrSlct === "PAPER")){
         outcome = "You Win! Scissors beats Paper";
-    } else if((plyrSlct == "SCISSORS") && (computerSelection == "ROCK")){
+    } else if((plyrSlct === "SCISSORS") && (cmptrSlct === "ROCK")){
         outcome = "You Lose! Rock beats Scissors";
-    } else if((plyrSlct == "PAPER") && (computerSelection == "SCISSORS")){
+    } else if((plyrSlct === "PAPER") && (cmptrSlct === "SCISSORS")){
         outcome = "You Lose! Scissors beats to Paper";
-    } else if((plyrSlct == "PAPER") && (computerSelection == "PAPER")){
+    } else if((plyrSlct === "PAPER") && (cmptrSlct === "PAPER")){
         outcome = "You Tie! Paper ties with Paper";
-    } else if((plyrSlct == "PAPER") && (computerSelection == "ROCK")){
+    } else if((plyrSlct === "PAPER") && (cmptrSlct === "ROCK")){
         outcome = "You Win! Paper beats Rock";
-    } else if((plyrSlct == "ROCK") && (computerSelection == "SCISSORS")){
+    } else if((plyrSlct === "ROCK") && (cmptrSlct === "SCISSORS")){
         outcome = "You Win! Rock beats Scissors";
-    } else if((plyrSlct == "ROCK") && (computerSelection == "PAPER")){
+    } else if((plyrSlct === "ROCK") && (cmptrSlct === "PAPER")){
         outcome = "You Lose! Paper beats Rock";
-    } else if((plyrSlct == "ROCK") && (computerSelection == "ROCK")){
+    } else if((plyrSlct === "ROCK") && (cmptrSlct === "ROCK")){
         outcome = "You Tie! Rock ties with Rock";
-    } else if((plyrSlct == "SCISSORS") && (computerSelection == "SCISSORS")){
+    } else if((plyrSlct === "SCISSORS") && (cmptrSlct === "SCISSORS")){
         outcome = "You Tie! Scissors ties with Scissors";
-    } 
+    } else (outcome ="N/A" + "| plyrSlct: " + plyrSlct + "| cmptrSlct: " + cmptrSlct);
+
+    //Calls chngTallie and prints new Tallie on screen
+    chngTallie(outcome);
+
+    console.log(outcome);
 
     //Outcome
     return outcome;
@@ -109,6 +115,23 @@ function dsplyCmptrSlct(){
 
 }
 
+// Change Tallie
+function chngTallie(outcome){
+    
+    if(outcome.includes("Win")){
+        wins = wins+1;
+        let plyrWins = document.querySelector('#plyrWins');
+        plyrWins.innerHTML = wins;
+    } else if(outcome.includes("Lose")){
+        pcWins = pcWins+1;
+        let cmptrWins = document.querySelector('#cmptrWins');
+        cmptrWins.innerHTML = pcWins;
+    } else return;
+
+    let plyrWins = document.querySelector('#plyrWins');
+}
+
+
 var resultsDiv = document.getElementById('results');
 
 function rmvDsply(){
@@ -121,33 +144,6 @@ function rmvDsply(){
         return;
     }
 ;}
-
-//5 Round game
-// function game(){
-
-//     //Call playRound 5 times via loop, and store results to each round aswell as keep counter of W,L,T
-//     for (let i=0; i<5; i++){
-
-//         playerSelectionFunct(); //New Player Selection
-//         computerPlay(); // New PC Selection
-
-//         roundResults.push(playRound(playerSelection, computerSelection))
-//         console.log("Round " + (i+1));
-//         console.log("PC Selection: " + computerSelection);
-//         console.log("Player Selection: " + playerSelection);
-//         console.log(roundResults[i]);
-//         //call win,loss & tie counters
-//         console.log("wins: " + winCounter());
-//         console.log("loses: " + lossCounter());
-//         console.log("ties: " + tieCounter());
-//         console.log("---------------")
-//         computerSelection = computerPlay(); //Change computer selection after round
-//     }
-
-//     let gameBreakdown = ("Wins: " + wins + " | Loses: " + loses + " | Ties: " + ties);
-//     console.log(gameBreakdown);
-//     console.log(gameResult()); //calls gameResult funct. Which is the total 5 round game outcome
-// }
 
 //Win, LOSS, TIE Trackers
     //Keep track of wins
@@ -162,9 +158,9 @@ function rmvDsply(){
     function lossCounter(){
         let rslt = playRound(playerSelection, computerSelection);
         if(rslt.includes("Lose")){
-            loses = loses+1;
+            pcWins = pcWins+1;
         };
-        return loses;
+        return pcWins;
     };
     //Keep track of ties
     function tieCounter(){
@@ -180,14 +176,42 @@ function gameResult(){
 
     // let gmresult = "";
 
-    if(wins>loses){
-        gmresult = ("You win the game " + wins + " to " + loses + " !");
-    } else if(wins<loses){
-        gmresult = ("You lose the game " + loses + " to " + wins + " !");
-    } else if(wins==loses){
-        gmresult = ("You tie " + wins + " to " + loses + " and " + ties + " ties");
+    if(wins>pcWins){
+        gmresult = ("You win the game " + wins + " to " + pcWins + " !");
+    } else if(wins<pcWins){
+        gmresult = ("You lose the game " + pcWins + " to " + wins + " !");
+    } else if(wins==pcWins){
+        gmresult = ("You tie " + wins + " to " + pcWins + " and " + ties + " ties");
     }
     return gmresult;
 }
+
+//5 Round game
+function game(){
+
+    //Call playRound 5 times via loop, and store results to each round aswell as keep counter of W,L,T
+    for (let i=0; i<5; i++){
+
+        playerSelectionFunct(); //New Player Selection
+        computerPlay(); // New PC Selection
+
+        roundResults.push(playRound(playerSelection, computerSelection))
+        console.log("Round " + (i+1));
+        console.log("PC Selection: " + computerSelection);
+        console.log("Player Selection: " + playerSelection);
+        console.log(roundResults[i]);
+        //call win,loss & tie counters
+        console.log("wins: " + winCounter());
+        console.log("pcWins: " + lossCounter());
+        console.log("ties: " + tieCounter());
+        console.log("---------------")
+        computerSelection = computerPlay(); //Change computer selection after round
+    }
+
+    let gameBreakdown = ("Wins: " + wins + " | pcWins: " + pcWins + " | Ties: " + ties);
+    console.log(gameBreakdown);
+    console.log(gameResult()); //calls gameResult funct. Which is the total 5 round game outcome
+}
+
 
 // game(); //CALL GAME
